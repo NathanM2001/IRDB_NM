81a4c4a4b094edc278da8e91405137b470f4a8e2
nathanmckenzie <ngm18@acu.edu>
2020-11-02 10:31:54 -0600

    Initial Commit

---
+++ IRDB_NM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IRDB_NM
+//
+//  Created by Nathan Mckenzie on 11/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
